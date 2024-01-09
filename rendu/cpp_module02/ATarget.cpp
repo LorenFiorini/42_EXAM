21259a9239794f924ce783409dc9cd4a90b30201
LorenFiorini <64514862+LorenFiorini@users.noreply.github.com>
2024-01-09 21:30:12 +0100

    Starting rank-05 problem 02

---
+++ rendu/cpp_module02/ATarget.cpp
@@ -0,0 +1,36 @@
+
+#include "ATarget.hpp"
+
+ATarget::ATarget(void) {
+	return ;
+}
+
+ATarget::ATarget(std::string type) : _type(type) {
+	return ;
+}
+
+ATarget::ATarget(ATarget const &obj) {
+	*this = obj;
+	return ;
+}
+
+ATarget::~ATarget(void) {
+	return ;
+}
+
+ATarget &ATarget::operator=(ATarget const &obj) {
+	if (this != &obj) {
+		this->_type = obj.getType();
+	}
+	return (*this);
+}
+
+std::string ATarget::getType(void) const {
+	return (this->_type);
+}
+
+void ATarget::getHitBySpell(ASpell const & spell) const {
+	std::cout << this->_type << " has been " << spell.getEffects() << "!" << std::endl;
+	return ;
+}
+
