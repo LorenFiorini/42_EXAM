edd868cf0420e6c725eb5ff72c5aa179281e5496
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2024-02-20 20:47:02 +0100

    reading mini_serv subject

--- rendu/mini_serv/mini_serv.c
+++ rendu/mini_serv/mini_serv.c
@@ -1,50 +1,19 @@
 
 #include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 
-
-// #define ERR_ARG_NUM_ENDL "Wrong number of arguments\n"
-// #define ERROR_FATAL_ENDL "Fatal error\n"
-
-
-
-int main(int argc, char ** argv) {
-	char * error_arg = "Wrong number of arguments\n";
-	char * error_fatal = "Fatal error\n";
-	if (argc != 2) {
-		write(2, error_arg, strlen(error_arg));
-		exit(1);
-	}
-	return (0);
+void ft_putstr_fd(char *str, int fd)
+{
+	write(fd, str, strlen(str));
 }
 
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		ft_putstr_fd("Wrong number of arguments\n", 2);
+		return (1);
+	}
 
-/*
-
-write
-close
-select
-socket
-accept
-listen
-send
-recv
-bind
-strstr
-
-malloc
-realloc
-free
-calloc
-bzero
-atoi
-sprintf
-strlen
-exit
-strcpy
-strcat
-memset
-
-*/
+	return (0);
+}
